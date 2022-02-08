47b369626d9cfdd469cf537f108cd3037a4d3a01
starsone <starsone@foxmail.com>
2022-02-08 11:14:52 +0800

    更新的module加个测试类

---
+++ update/src/main/java/com/tyky/update/UpdateCheck.java
@@ -0,0 +1,10 @@
+package com.tyky.update;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+public class UpdateCheck {
+    public static void test(Activity activity) {
+        Toast.makeText(activity, "测试！！", Toast.LENGTH_SHORT).show();
+    }
+}
