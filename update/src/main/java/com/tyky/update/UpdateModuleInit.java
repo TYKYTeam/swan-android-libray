47ad8ddec45f5f9d3beac16ab3d7b9dc7fbe2a22
starsone <starsone@foxmail.com>
2022-03-01 17:04:10 +0800

    update模块创建module初始化类

---
+++ update/src/main/java/com/tyky/update/UpdateModuleInit.java
@@ -0,0 +1,15 @@
+package com.tyky.update;
+
+import android.app.Application;
+
+import com.tyky.webviewBase.annotation.ApplicationInit;
+import com.tyky.webviewBase.interf.ModuleInit;
+
+@ApplicationInit
+public class UpdateModuleInit implements ModuleInit {
+
+    @Override
+    public void init(Application application) {
+
+    }
+}
