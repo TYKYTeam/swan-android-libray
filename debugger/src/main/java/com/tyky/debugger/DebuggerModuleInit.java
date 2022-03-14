9088df3626ee5537aa4015c11b8193487d6e7b00
starsone <starsone@foxmail.com>
2022-03-14 15:15:07 +0800

    Bugly的更新模块引入（还需要测试）

--- debugger/src/main/java/com/tyky/debugger/DebuggerModuleInit.java
+++ debugger/src/main/java/com/tyky/debugger/DebuggerModuleInit.java
@@ -2,7 +2,8 @@ package com.tyky.debugger;
 
 import android.app.Application;
 
-import com.tencent.bugly.crashreport.CrashReport;
+import com.blankj.utilcode.util.MetaDataUtils;
+import com.tencent.bugly.Bugly;
 import com.tyky.webviewBase.annotation.ApplicationInit;
 import com.tyky.webviewBase.interf.ModuleInit;
 
@@ -12,6 +13,8 @@ public class DebuggerModuleInit implements ModuleInit {
     @Override
     public void init(Application application) {
         //Bugly初始化
-        CrashReport.initCrashReport(application);
+        String appId = MetaDataUtils.getMetaDataInApp("BUGLY_APPID");
+        //最后的参数代表是否开启debuger模式
+        Bugly.init(application,appId,true);
     }
 }
