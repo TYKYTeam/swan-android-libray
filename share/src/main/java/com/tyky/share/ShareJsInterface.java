b28e322d6877e73ed1e40a0fa14a78ce0877013a
starsone <starsone@foxmail.com>
2022-03-14 16:20:21 +0800

    新增分享文本给微信好友

--- share/src/main/java/com/tyky/share/ShareJsInterface.java
+++ share/src/main/java/com/tyky/share/ShareJsInterface.java
@@ -1,10 +1,13 @@
 package com.tyky.share;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.text.TextUtils;
 import android.webkit.JavascriptInterface;
 
 import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.EncodeUtils;
 import com.blankj.utilcode.util.GsonUtils;
 import com.blankj.utilcode.util.ImageUtils;
@@ -100,5 +103,29 @@ public class ShareJsInterface {
         return gson.toJson(ResultModel.success(""));
     }
 
+    /**
+     * 直接分享文本到微信好友
+     */
+    @JavascriptInterface
+    public String shareToWechatFriend(String paramStr) {
+        ParamModel paramModel = gson.fromJson(paramStr, ParamModel.class);
+        String content = paramModel.getContent();
+        if (StringUtils.isEmpty(content)) {
+            return gson.toJson(ResultModel.errorParam());
+        }
+        String weixinPkg = "com.tencent.mm";
+        if (!AppUtils.isAppInstalled(weixinPkg)) {
+            return gson.toJson(ResultModel.errorParam("分析失败，微信未安装！！"));
+        }
+        Intent intent = new Intent();
+        ComponentName cop = new ComponentName(weixinPkg, "com.tencent.mm.ui.tools.ShareImgUI");
+        intent.setComponent(cop);
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra("android.intent.extra.TEXT", content);
+        intent.putExtra("Kdescription", !TextUtils.isEmpty(content) ? content : "");
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        ActivityUtils.getTopActivity().startActivity(intent);
+        return gson.toJson(ResultModel.success(""));
+    }
 
 }
