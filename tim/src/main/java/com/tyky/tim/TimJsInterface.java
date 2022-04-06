28d36f3d303e04fcf35d1055589bfd34d936464d
starsone <starsone@foxmail.com>
2022-04-06 15:19:55 +0800

    新增IM模块的获取消息未读数js接口

--- tim/src/main/java/com/tyky/tim/TimJsInterface.java
+++ tim/src/main/java/com/tyky/tim/TimJsInterface.java
@@ -8,10 +8,15 @@ import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.GsonUtils;
 import com.blankj.utilcode.util.StringUtils;
 import com.google.gson.Gson;
+import com.tencent.imsdk.v2.V2TIMManagerImpl;
+import com.tencent.imsdk.v2.V2TIMValueCallback;
 import com.tencent.qcloud.tim.demo.SplashActivity;
 import com.tyky.webviewBase.annotation.WebViewInterface;
+import com.tyky.webviewBase.event.JsCallBackEvent;
 import com.tyky.webviewBase.model.ParamModel;
 import com.tyky.webviewBase.model.ResultModel;
+
+import org.greenrobot.eventbus.EventBus;
 
 @WebViewInterface("tim")
 public class TimJsInterface {
@@ -59,4 +64,29 @@ public class TimJsInterface {
         return gson.toJson(ResultModel.success(""));
     }
 
+    /**
+     * 获取当前用户未读数
+     * @return
+     */
+    @JavascriptInterface
+    public String getUnreadCount(String paramStr) {
+        ParamModel paramModel = gson.fromJson(paramStr, ParamModel.class);
+        String methodName = paramModel.getCallBackMethod();
+        if (StringUtils.isEmpty(methodName)) {
+            return gson.toJson(ResultModel.errorParam(""));
+        }
+        V2TIMManagerImpl.getConversationManager().getTotalUnreadMessageCount(new V2TIMValueCallback<Long>() {
+            @Override
+            public void onSuccess(Long aLong) {
+                EventBus.getDefault().post(new JsCallBackEvent(methodName,ResultModel.success(aLong)));
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                String text = "会话未读数目错误:" + i + " " + s;
+                EventBus.getDefault().post(new JsCallBackEvent(methodName,ResultModel.errorParam(text)));
+            }
+        });
+        return gson.toJson(ResultModel.success(""));
+    }
 }
