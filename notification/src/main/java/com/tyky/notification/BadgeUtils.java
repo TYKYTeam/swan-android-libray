93fb0d57abae50925120a2d9d8b2f86e5241fddd
starsone <starsone@foxmail.com>
2022-04-14 15:50:25 +0800

    新增小红点的工具类封装，增加对应机型小红点需要权限

---
+++ notification/src/main/java/com/tyky/notification/BadgeUtils.java
@@ -0,0 +1,324 @@
+package com.tyky.notification;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.AsyncQueryHandler;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+
+import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.ResourceUtils;
+import com.blankj.utilcode.util.ToastUtils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+/**
+ * 应用角标工具类
+ *
+ * @author stars-one
+ * @since 2022-4-12 18:17:09
+ */
+public class BadgeUtils {
+    private static int notificationId = 0;
+
+    public static boolean setCount(@Nullable Activity activity, @Nullable Integer count) {
+
+        if (count >= 0) {
+            switch (Build.BRAND.toLowerCase()) {
+                case "xiaomi":
+                    new Handler().postDelayed(() -> setNotificationBadge(activity,count), 3000);
+                    ToastUtils.showShort("请切到后台，3秒后会收到通知");
+                    return true;
+                case "huawei":
+                case "honor":
+                    return setHuaweiBadge(activity,count);
+                case "samsung":
+                    return setSamsungBadge(activity,count);
+                case "oppo":
+                    return setOPPOBadge(activity,count) || setOPPOBadge2(activity,count);
+                case "vivo":
+                    return setVivoBadge(activity,count);
+                case "lenovo":
+                    return setZukBadge(activity,count);
+                case "htc":
+                    return setHTCBadge(activity,count);
+                case "sony":
+                    return setSonyBadge(activity,count);
+                default:
+                    return setNotificationBadge(activity,count);
+            }
+        } else {
+            return false;
+        }
+    }
+
+
+    public static boolean setNotificationBadge(@Nullable Activity activity,@Nullable Integer count) {
+        NotificationManager notificationManager = (NotificationManager) activity.getSystemService
+                (Context.NOTIFICATION_SERVICE);
+        if (notificationManager == null) {
+            return false;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 8.0之后添加角标需要NotificationChannel
+            NotificationChannel channel = new NotificationChannel("badge", "badge",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setShowBadge(true);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(activity, activity.getClass());
+        PendingIntent pendingIntent = PendingIntent.getActivity(activity, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(ActivityUtils.getTopActivity(), "badge")
+                .setContentTitle("应用角标")
+                .setContentText("您有" + count + "条未读消息")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(ResourceUtils.getMipmapIdByName("ic_launcher"))
+                .setLargeIcon(BitmapFactory.decodeResource(ActivityUtils.getTopActivity().getResources(), ResourceUtils.getMipmapIdByName("ic_launcher")))
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setChannelId("badge")
+                .setNumber(count)
+                .setBadgeIconType(NotificationCompat.BADGE_ICON_SMALL).build();
+        // 小米
+        if (Build.BRAND.equalsIgnoreCase("xiaomi")) {
+            setXiaomiBadge(count, notification);
+        }
+        notificationManager.notify(notificationId++, notification);
+        return true;
+    }
+
+    private static void setXiaomiBadge(int count, Notification notification) {
+        try {
+            Field field = notification.getClass().getDeclaredField("extraNotification");
+            Object extraNotification = field.get(notification);
+            Method method = extraNotification.getClass().getDeclaredMethod("setMessageCount", int
+                    .class);
+            method.invoke(extraNotification, count);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean setHuaweiBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            String launchClassName = getLauncherClassName(activity);
+            if (TextUtils.isEmpty(launchClassName)) {
+                return false;
+            }
+            Bundle bundle = new Bundle();
+            bundle.putString("package", activity.getPackageName());
+            bundle.putString("class", launchClassName);
+            bundle.putInt("badgenumber", count);
+            activity.getContentResolver().call(Uri.parse("content://com.huawei.android.launcher" +
+                    ".settings/badge/"), "change_badge", null, bundle);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean setSamsungBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            String launcherClassName = getLauncherClassName(activity);
+            if (TextUtils.isEmpty(launcherClassName)) {
+                return false;
+            }
+            Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
+            intent.putExtra("badge_count", count);
+            intent.putExtra("badge_count_package_name", activity.getPackageName());
+            intent.putExtra("badge_count_class_name", launcherClassName);
+            activity.sendBroadcast(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Deprecated
+    private static boolean setOPPOBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            Bundle extras = new Bundle();
+            extras.putInt("app_badge_count", count);
+            activity.getContentResolver().call(Uri.parse("content://com.android.badge/badge"),
+                    "setAppBadgeCount", String.valueOf(count), extras);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Deprecated
+    private static boolean setOPPOBadge2(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            Intent intent = new Intent("com.oppo.unsettledevent");
+            intent.putExtra("packageName", activity.getPackageName());
+            intent.putExtra("number", count);
+            intent.putExtra("upgradeNumber", count);
+            PackageManager packageManager = activity.getPackageManager();
+            List<ResolveInfo> receivers = packageManager.queryBroadcastReceivers(intent, 0);
+            if (receivers != null && receivers.size() > 0) {
+                activity.sendBroadcast(intent);
+            } else {
+                Bundle extras = new Bundle();
+                extras.putInt("app_badge_count", count);
+                activity.getContentResolver().call(Uri.parse("content://com.android.badge/badge"),
+                        "setAppBadgeCount", null, extras);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private static boolean setVivoBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            String launcherClassName = getLauncherClassName(activity);
+            if (TextUtils.isEmpty(launcherClassName)) {
+                return false;
+            }
+            Intent intent = new Intent("launcher.action.CHANGE_APPLICATION_NOTIFICATION_NUM");
+            intent.putExtra("packageName", activity.getPackageName());
+            intent.putExtra("className", launcherClassName);
+            intent.putExtra("notificationNum", count);
+            intent.addFlags(0x01000000);
+            activity.sendBroadcast(intent);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean setZukBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            Bundle extra = new Bundle();
+            ArrayList<String> ids = new ArrayList<>();
+            // 以列表形式传递快捷方式id，可以添加多个快捷方式id
+//        ids.add("custom_id_1");
+//        ids.add("custom_id_2");
+            extra.putStringArrayList("app_shortcut_custom_id", ids);
+            extra.putInt("app_badge_count", count);
+            Uri contentUri = Uri.parse("content://com.android.badge/badge");
+            Bundle bundle = activity.getContentResolver().call(contentUri, "setAppBadgeCount", null,
+                    extra);
+            return bundle != null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean setHTCBadge(@Nullable Activity activity,@Nullable Integer count) {
+        try {
+            ComponentName launcherComponentName = getLauncherComponentName(activity);
+            if (launcherComponentName == null) {
+                return false;
+            }
+
+            Intent intent1 = new Intent("com.htc.launcher.action.SET_NOTIFICATION");
+            intent1.putExtra("com.htc.launcher.extra.COMPONENT", launcherComponentName
+                    .flattenToShortString());
+            intent1.putExtra("com.htc.launcher.extra.COUNT", count);
+            activity.sendBroadcast(intent1);
+
+            Intent intent2 = new Intent("com.htc.launcher.action.UPDATE_SHORTCUT");
+            intent2.putExtra("packagename", launcherComponentName.getPackageName());
+            intent2.putExtra("count", count);
+            activity.sendBroadcast(intent2);
+
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean setSonyBadge(@Nullable Activity activity,@Nullable Integer count) {
+        String launcherClassName = getLauncherClassName(activity);
+        if (TextUtils.isEmpty(launcherClassName)) {
+            return false;
+        }
+        try {
+            //官方给出方法
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("badge_count", count);
+            contentValues.put("package_name", activity.getPackageName());
+            contentValues.put("activity_name", launcherClassName);
+            SonyAsyncQueryHandler asyncQueryHandler = new SonyAsyncQueryHandler(activity
+                    .getContentResolver());
+            asyncQueryHandler.startInsert(0, null, Uri.parse("content://com.sonymobile.home" +
+                    ".resourceprovider/badge"), contentValues);
+            return true;
+        } catch (Exception e) {
+            try {
+                //网上大部分使用方法
+                Intent intent = new Intent("com.sonyericsson.home.action.UPDATE_BADGE");
+                intent.putExtra("com.sonyericsson.home.intent.extra.badge.SHOW_MESSAGE", count > 0);
+                intent.putExtra("com.sonyericsson.home.intent.extra.badge.ACTIVITY_NAME",
+                        launcherClassName);
+                intent.putExtra("com.sonyericsson.home.intent.extra.badge.MESSAGE", String
+                        .valueOf(count));
+                intent.putExtra("com.sonyericsson.home.intent.extra.badge.PACKAGE_NAME", activity
+                        .getPackageName());
+                activity.sendBroadcast(intent);
+                return true;
+            } catch (Exception e1) {
+                e1.printStackTrace();
+                return false;
+            }
+        }
+    }
+
+    private static String getLauncherClassName(Context context) {
+        ComponentName launchComponent = getLauncherComponentName(context);
+        if (launchComponent == null) {
+            return "";
+        } else {
+            return launchComponent.getClassName();
+        }
+    }
+
+    private static ComponentName getLauncherComponentName(Context context) {
+        Intent launchIntent = context.getPackageManager().getLaunchIntentForPackage(context
+                .getPackageName());
+        if (launchIntent != null) {
+            return launchIntent.getComponent();
+        } else {
+            return null;
+        }
+    }
+
+    static class SonyAsyncQueryHandler extends AsyncQueryHandler {
+
+        SonyAsyncQueryHandler(ContentResolver cr) {
+            super(cr);
+        }
+    }
+}
