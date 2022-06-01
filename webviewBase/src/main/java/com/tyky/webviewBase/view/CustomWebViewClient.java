12202e6074eb606d1d9419c2792edc44842f2a62
starsone <starsone@foxmail.com>
2022-06-01 15:51:56 +0800

    webviewBase扩展拨打电话和发送短信

--- webviewBase/src/main/java/com/tyky/webviewBase/view/CustomWebViewClient.java
+++ webviewBase/src/main/java/com/tyky/webviewBase/view/CustomWebViewClient.java
@@ -1,9 +1,12 @@
 package com.tyky.webviewBase.view;
 
+import android.annotation.SuppressLint;
 import android.os.Build;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.blankj.utilcode.util.PhoneUtils;
 
 import androidx.annotation.RequiresApi;
 
@@ -13,15 +16,34 @@ public class CustomWebViewClient extends WebViewClient {
      */
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        view.loadUrl(url);
+        handleUrl(view, url);
         return true;
     }
 
+    /**
+     * 5.0版本以上都走这个方法
+     *
+     * @param view
+     * @param request
+     * @return
+     */
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-        view.loadUrl(request.getUrl().toString());
+        String url = request.getUrl().toString();
+        handleUrl(view, url);
         return true;
     }
 
+    @SuppressLint("MissingPermission")
+    private void handleUrl(WebView view, String url) {
+        String phone = url.substring(4);
+        if (url.contains("tel")) {
+            PhoneUtils.dial(phone);
+        } else if (url.contains("sms")) {
+            PhoneUtils.sendSms(phone, "");
+        } else {
+            view.loadUrl(url);
+        }
+    }
 }
