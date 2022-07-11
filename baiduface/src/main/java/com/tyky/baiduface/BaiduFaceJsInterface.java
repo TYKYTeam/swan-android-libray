41b22c223b95d42609acbe65d8cbd03b639ed25d
starsone <starsone@foxmail.com>
2022-07-11 17:04:35 +0800

    :mute: 移除百度人脸接口的日志打印

--- baiduface/src/main/java/com/tyky/baiduface/BaiduFaceJsInterface.java
+++ baiduface/src/main/java/com/tyky/baiduface/BaiduFaceJsInterface.java
@@ -1,12 +1,10 @@
 package com.tyky.baiduface;
 
-import android.util.Log;
 import android.webkit.JavascriptInterface;
 
 import com.baidu.idl.face.example.HomeActivity;
 import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.GsonUtils;
-import com.blankj.utilcode.util.ToastUtils;
 import com.google.gson.Gson;
 import com.tyky.webviewBase.annotation.WebViewInterface;
 import com.tyky.webviewBase.model.ParamModel;
@@ -25,8 +23,6 @@ public class BaiduFaceJsInterface {
      */
     @JavascriptInterface
     public String facialRecognite(String paramStr) {
-        Log.d("test", "进入");
-        ToastUtils.showShort("进入yy");
         ParamModel paramModel = gson.fromJson(paramStr, ParamModel.class);
         ActivityUtils.startActivity(HomeActivity.class);
         return gson.toJson(ResultModel.success(""));
