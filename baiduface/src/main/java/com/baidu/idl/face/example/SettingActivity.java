458f4e3c3fb7de96565f92ff26e8bd467dc1f549
starsone <starsone@foxmail.com>
2022-07-11 16:44:49 +0800

    修复百度人脸页面点击设置无法进入设置页面的问题

--- baiduface/src/main/java/com/baidu/idl/face/example/SettingActivity.java
+++ baiduface/src/main/java/com/baidu/idl/face/example/SettingActivity.java
@@ -57,7 +57,7 @@ public class SettingActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_face_setting);
         initView();
         initData();
         initListener();
