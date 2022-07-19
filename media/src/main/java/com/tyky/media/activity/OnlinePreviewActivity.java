32d2cddbfe8b2cb3b6c9686a1de0441d4f17c753
starsone <starsone@foxmail.com>
2022-07-19 11:24:31 +0800

    :ambulance: 修复在线预览新开页面webview加载白屏的问题

--- media/src/main/java/com/tyky/media/activity/OnlinePreviewActivity.java
+++ media/src/main/java/com/tyky/media/activity/OnlinePreviewActivity.java
@@ -1,12 +1,8 @@
 package com.tyky.media.activity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.webkit.WebChromeClient;
-import android.widget.Button;
+import android.webkit.WebSettings;
 
-import com.blankj.utilcode.util.ThreadUtils;
-import com.blankj.utilcode.util.ToastUtils;
 import com.tyky.media.R;
 import com.tyky.webviewBase.view.CustomWebView;
 
@@ -14,34 +10,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class OnlinePreviewActivity extends AppCompatActivity {
 
+    private CustomWebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_preview);
-
         String url = getIntent().getStringExtra("url");
-        CustomWebView webView = findViewById(R.id.webview);
-        webView.setWebChromeClient(new WebChromeClient());
-
-        ThreadUtils.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                webView.loadUrl("file:///android_asset/index_old.html");
-            }
-        });
-
-        //url = "https://stars-one.site";
-        //if (!StringUtils.isEmpty(url)) {
-        //}
-        Button button = findViewById(R.id.btnTest);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ToastUtils.showShort("点击。。");
-                String my = "https://www.cnblogs.com/stars-one";
-                webView.loadUrl(my);
-            }
-        });
-
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(true);
+        webView.loadUrl(url);
     }
 }
