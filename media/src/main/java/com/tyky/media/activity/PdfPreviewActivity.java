79b3f37f94a5f45c0c1a383c11acec953c8ebd22
starsone <starsone@foxmail.com>
2022-07-19 11:56:31 +0800

    :zap: 优化pdf预览的加载效果

--- media/src/main/java/com/tyky/media/activity/PdfPreviewActivity.java
+++ media/src/main/java/com/tyky/media/activity/PdfPreviewActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.blankj.utilcode.util.PathUtils;
 import com.blankj.utilcode.util.ResourceUtils;
+import com.blankj.utilcode.util.ThreadUtils;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.kongzue.dialogx.dialogs.WaitDialog;
 import com.tyky.media.R;
@@ -21,24 +22,27 @@ public class PdfPreviewActivity extends AppCompatActivity {
 
         PDFView pdfView = findViewById(R.id.pdfView);
 
-        File pdfFile = new File(PathUtils.getExternalAppCachePath(), "test.pdf");
-        ResourceUtils.copyFileFromAssets("test.pdf", pdfFile.getPath());
-
         WaitDialog dialog = WaitDialog.show("加载中");
+        ThreadUtils.getIoPool().submit(() -> {
+            File pdfFile = new File(PathUtils.getExternalAppCachePath(), "test.pdf");
+            ResourceUtils.copyFileFromAssets("test.pdf", pdfFile.getPath());
 
-        pdfView.fromFile(pdfFile)   //设置pdf文件地址
-                .defaultPage(0)
-                .swipeHorizontal(false)
-                .enableSwipe(false)
-                .enableDoubletap(true)
-                .enableAnnotationRendering(true)
-                //.scrollHandle(new DefaultScrollHandle(PdfShowActivity.this))
-                .spacing(10) // in dp
-                .onLoad(nbPages -> dialog.doDismiss())
-                .load();
+            runOnUiThread(() -> {
+                pdfView.fromFile(pdfFile)   //设置pdf文件地址
+                        .defaultPage(0)
+                        .swipeHorizontal(false)
+                        .enableSwipe(false)
+                        .enableDoubletap(true)
+                        .enableAnnotationRendering(true)
+                        //.scrollHandle(new DefaultScrollHandle(PdfShowActivity.this))
+                        .spacing(10) // in dp
+                        .onLoad(nbPages -> dialog.doDismiss())
+                        .load();
 
-        pdfView.fitToWidth();
-        pdfView.zoomWithAnimation(3f);
+                pdfView.fitToWidth();
+                pdfView.zoomWithAnimation(1f);
+            });
+        });
 
 
     }
