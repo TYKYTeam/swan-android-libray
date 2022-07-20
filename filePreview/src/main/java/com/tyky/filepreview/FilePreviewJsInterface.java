7dc4d654e69dc45184fdc8b573700710d08b7375
wanxingxing <wanxingxing@linewell.com>
2022-07-20 10:51:12 +0800

    :sparkles: 新增pdf本地预览的功能（传递pdf文件下载地址或base64字符串）

--- filePreview/src/main/java/com/tyky/filepreview/FilePreviewJsInterface.java
+++ filePreview/src/main/java/com/tyky/filepreview/FilePreviewJsInterface.java
@@ -1,13 +1,20 @@
 package com.tyky.filepreview;
 
+import android.os.Bundle;
+import android.text.TextUtils;
 import android.webkit.JavascriptInterface;
 
 import com.blankj.utilcode.util.ActivityUtils;
+import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.PathUtils;
 import com.google.gson.Gson;
 import com.tyky.filepreview.activity.PdfPreviewActivity;
 import com.tyky.webviewBase.annotation.WebViewInterface;
+import com.tyky.webviewBase.model.ParamModel;
 import com.tyky.webviewBase.model.ResultModel;
+
+import java.io.File;
 
 @WebViewInterface("filePreview")
 public class FilePreviewJsInterface {
@@ -15,11 +22,22 @@ public class FilePreviewJsInterface {
     Gson gson = GsonUtils.getGson();
 
     /**
-     * 预览文件（不包含图片）
+     * 预览pdf文件
      */
     @JavascriptInterface
     public String previewPdf(String paramStr) {
-        ActivityUtils.startActivity(PdfPreviewActivity.class);
+        ParamModel paramModel = gson.fromJson(paramStr, ParamModel.class);
+        String content = paramModel.getContent();
+        if (TextUtils.isEmpty(content)) {
+            return gson.toJson(ResultModel.errorParam());
+        }
+        //将文件内容写入文件，之后再读取（防止数据过大导致Intent传递数据失败）
+        File file = new File(PathUtils.getExternalAppCachePath(), System.currentTimeMillis() + ".txt");
+        FileIOUtils.writeFileFromString(file, content);
+        //文件路径存在bundle中
+        Bundle bundle = new Bundle();
+        bundle.putString("filePath", file.getPath());
+        ActivityUtils.startActivity(bundle, PdfPreviewActivity.class);
         return gson.toJson(ResultModel.success(""));
     }
 }
--- filePreview/src/main/java/com/tyky/filepreview/activity/PdfPreviewActivity.java
+++ filePreview/src/main/java/com/tyky/filepreview/activity/PdfPreviewActivity.java
@@ -2,14 +2,19 @@ package com.tyky.filepreview.activity;
 
 import android.os.Bundle;
 
-import com.blankj.utilcode.util.PathUtils;
-import com.blankj.utilcode.util.ResourceUtils;
+import com.blankj.utilcode.util.EncodeUtils;
+import com.blankj.utilcode.util.FileIOUtils;
 import com.blankj.utilcode.util.ThreadUtils;
+import com.blankj.utilcode.util.ToastUtils;
 import com.github.barteksc.pdfviewer.PDFView;
 import com.kongzue.dialogx.dialogs.WaitDialog;
+import com.socks.library.KLog;
 import com.tyky.filepreview.R;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,30 +25,65 @@ public class PdfPreviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_preview);
 
+        String filePath = getIntent().getStringExtra("filePath");
+        String content = FileIOUtils.readFile2String(filePath);
+
         PDFView pdfView = findViewById(R.id.pdfView);
 
         WaitDialog dialog = WaitDialog.show("加载中");
-        ThreadUtils.getIoPool().submit(() -> {
-            File pdfFile = new File(PathUtils.getExternalAppCachePath(), "test.pdf");
-            ResourceUtils.copyFileFromAssets("test.pdf", pdfFile.getPath());
+        if (content.startsWith("http")) {
+            ThreadUtils.getIoPool().submit(() -> {
 
-            runOnUiThread(() -> {
-                pdfView.fromFile(pdfFile)   //设置pdf文件地址
-                        .defaultPage(0)
-                        .swipeHorizontal(false)
-                        .enableSwipe(false)
-                        .enableDoubletap(true)
-                        .enableAnnotationRendering(true)
-                        //.scrollHandle(new DefaultScrollHandle(PdfShowActivity.this))
-                        .spacing(10) // in dp
-                        .onLoad(nbPages -> dialog.doDismiss())
-                        .load();
+                try {
+                    URLConnection urlConnection = new URL(content).openConnection();
+                    InputStream inputStream = urlConnection.getInputStream();
+                    runOnUiThread(() -> {
+                        pdfView.fromStream(inputStream)   //从链接加载pdf文件预览
+                                .defaultPage(0)
+                                .swipeHorizontal(false)
+                                .enableSwipe(false)
+                                .enableDoubletap(true)
+                                .enableAnnotationRendering(true)
+                                //.scrollHandle(new DefaultScrollHandle(PdfShowActivity.this))
+                                .spacing(10) // in dp
+                                .onLoad(nbPages -> dialog.doDismiss())
+                                .load();
 
-                pdfView.fitToWidth();
-                pdfView.zoomWithAnimation(1f);
+                        pdfView.fitToWidth();
+                        pdfView.zoomWithAnimation(1f);
+                    });
+                } catch (IOException e) {
+                    runOnUiThread(() -> {
+                        KLog.e("下载失败："+e.getMessage());
+                        dialog.doDismiss();
+                        ToastUtils.showShort("下载pdf文件失败，原因" + e.getMessage());
+                    });
+                }
+
             });
-        });
+        } else {
+            //base64字符串 未测试！
+            ThreadUtils.getIoPool().submit(() -> {
+                byte[] bytes = EncodeUtils.base64Decode(content);
 
+                runOnUiThread(() -> {
+                    pdfView.fromBytes(bytes)   //设置pdf文件地址
+                            .defaultPage(0)
+                            .swipeHorizontal(false)
+                            .enableSwipe(false)
+                            .enableDoubletap(true)
+                            .enableAnnotationRendering(true)
+                            //.scrollHandle(new DefaultScrollHandle(PdfShowActivity.this))
+                            .spacing(10) // in dp
+                            .onLoad(nbPages -> dialog.doDismiss())
+                            .load();
+
+                    pdfView.fitToWidth();
+                    pdfView.zoomWithAnimation(1f);
+                });
+            });
+
+        }
 
     }
 }
