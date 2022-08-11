cfd40218eecb176f6964cd81b85753f6e96091ce
wanxingxing <wanxingxing@linewell.com>
2022-08-11 10:20:01 +0800

    :sparkles: 新增热更新资源包的下载和解压流程

--- update/src/main/java/com/tyky/update/dialog/CheckVersionDialog.java
+++ update/src/main/java/com/tyky/update/dialog/CheckVersionDialog.java
@@ -7,6 +7,7 @@ import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.AppUtils;
 import com.blankj.utilcode.util.PathUtils;
 import com.blankj.utilcode.util.ToastUtils;
+import com.blankj.utilcode.util.ZipUtils;
 import com.kongzue.baseokhttp.HttpRequest;
 import com.kongzue.baseokhttp.listener.OnDownloadListener;
 import com.kongzue.dialogx.dialogs.MessageDialog;
@@ -15,13 +16,11 @@ import com.socks.library.KLog;
 import com.tyky.update.bean.UpdateParamModel;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CheckVersionDialog {
 
     public static void show(UpdateParamModel paramModel) {
-
-        //获取当前APP版本号和h5版本号
-        int currentVersionCode = AppUtils.getAppVersionCode();
 
         int versionCode = paramModel.getVersionCode();
         int h5VersionCode = paramModel.getH5VersionCode();
@@ -37,9 +36,9 @@ public class CheckVersionDialog {
         }*/
 
         if (type == 1) {
-            //热更新
+            //热更新，直接静默下载，不给提示
+            downloadFile(paramModel);
         } else {
-
 
             if (forceUpdate) {
                 //是强制更新
@@ -81,7 +80,56 @@ public class CheckVersionDialog {
         int h5VersionCode = updateParamModel.getH5VersionCode();
 
         if (type == 1) {
-            //todo 下载h5资源包
+            //下载h5资源包
+            File file = new File(PathUtils.getExternalAppFilesPath(), "temp_" + h5VersionCode + ".zip");
+            HttpRequest.DOWNLOAD(
+                    ActivityUtils.getTopActivity(), downloadUrl,
+                    file,
+                    new OnDownloadListener() {
+                        @Override
+                        public void onDownloadSuccess(File file) {
+                            if (file.length() > 0) {
+                                // 解压操作
+                                File dir = new File(PathUtils.getExternalAppFilesPath(), "h5assetes/" + versionCode + "/" + h5VersionCode);
+                                if (!dir.exists()) {
+                                    dir.mkdirs();
+                                }
+
+                                try {
+                                    ZipUtils.unzipFile(file, dir);
+
+                                    boolean needTip = updateParamModel.isNeedTip();
+                                    if (needTip) {
+                                        MessageDialog.show("提示", "请点击确定，重启APP完成更新", "确定").setOkButtonClickListener(new OnDialogButtonClickListener<MessageDialog>() {
+                                            @Override
+                                            public boolean onClick(MessageDialog baseDialog, View v) {
+                                                AppUtils.relaunchApp(true);
+                                                return false;
+                                            }
+                                        });
+                                    }
+
+                                } catch (IOException e) {
+                                    KLog.e("解压失败"+e.getMessage());
+                                }
+
+                            } else {
+                                ToastUtils.showShort("apk文件大小为0KB，更新失败！");
+                            }
+                        }
+
+                        @Override
+                        public void onDownloading(int progress) {
+                            KLog.d("下载进度：" + progress);
+                        }
+
+                        @Override
+                        public void onDownloadFailed(Exception e) {
+                            ToastUtils.showShort("下载失败，原因：" + e.getMessage());
+                        }
+                    }
+            );
+
         } else {
             File file = new File(PathUtils.getExternalAppFilesPath(), "temp_" + versionCode + ".apk");
 
