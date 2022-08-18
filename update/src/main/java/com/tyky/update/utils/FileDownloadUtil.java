7d045bec1613a2b7c6889fc16d3f9c3562bdaeae
wanxingxing <wanxingxing@linewell.com>
2022-08-18 11:23:35 +0800

    :sparkles: 优化逻辑（判断服务端不支持断点更新的走正常的重新下载逻辑）

--- update/src/main/java/com/tyky/update/utils/FileDownloadUtil.java
+++ update/src/main/java/com/tyky/update/utils/FileDownloadUtil.java
@@ -1,8 +1,11 @@
 package com.tyky.update.utils;
 
+import android.text.TextUtils;
+
 import com.blankj.utilcode.util.ThreadUtils;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.RandomAccessFile;
@@ -41,40 +44,80 @@ public class FileDownloadUtil {
                     Response resp = call.execute();
 
                     double length = Long.parseLong(resp.header("Content-Length")) * 1.0;
-                    InputStream fis = resp.body().byteStream();
-                    ReadableByteChannel fisChannel = Channels.newChannel(fis);
+                    FileChannel foschannel ;
 
-                    RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
-                    //从上次未完成的位置开始下载
-                    randomAccessFile.seek(startSize);
-                    FileChannel foschannel = randomAccessFile.getChannel();
+                    if (!TextUtils.isEmpty(resp.header("Content-Range"))) {
+                        //断点继续下载
+                        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+                        //从上次未完成的位置开始下载
+                        randomAccessFile.seek(startSize);
+                        randomAccessFile.getChannel();
+                        foschannel = randomAccessFile.getChannel();
+                        InputStream fis = resp.body().byteStream();
+                        ReadableByteChannel fisChannel = Channels.newChannel(fis);
 
-                    // 通道没有办法传输数据，必须依赖缓冲区
-                    // 分配指定大小的缓冲区
-                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                        // 通道没有办法传输数据，必须依赖缓冲区
+                        // 分配指定大小的缓冲区
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
-                    // 将通道中的数据存入缓冲区中
-                    while (fisChannel.read(byteBuffer) != -1) {  // fisChannel 中的数据读到 byteBuffer 缓冲区中
-                        byteBuffer.flip();  // 切换成读数据模式
-                        // 将缓冲区中的数据写入通道
-                        foschannel.write(byteBuffer);
+                        // 将通道中的数据存入缓冲区中
+                        while (fisChannel.read(byteBuffer) != -1) {  // fisChannel 中的数据读到 byteBuffer 缓冲区中
+                            byteBuffer.flip();  // 切换成读数据模式
+                            // 将缓冲区中的数据写入通道
+                            foschannel.write(byteBuffer);
 
-                        final double progress = (foschannel.size() / length);
-                        BigDecimal two = new BigDecimal(progress);
-                        double result = two.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
-                        //计算进度，回调
-                        if (listener != null) {
-                            listener.onProgress(result);
+                            final double progress = (foschannel.size() / length);
+                            BigDecimal two = new BigDecimal(progress);
+                            double result = two.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+                            //计算进度，回调
+                            if (listener != null) {
+                                listener.onProgress(result);
+                            }
+                            byteBuffer.clear();  // 清空缓冲区
                         }
-                        byteBuffer.clear();  // 清空缓冲区
-                    }
-                    foschannel.close();
-                    fisChannel.close();
-                    randomAccessFile.close();
+                        foschannel.close();
+                        fisChannel.close();
+                        randomAccessFile.close();
 
-                    if (listener != null) {
-                        listener.onSuccess(file);
+                        if (listener != null) {
+                            listener.onSuccess(file);
+                        }
+                    } else {
+                        //走重新下载的逻辑
+                        FileOutputStream fileOutputStream = new FileOutputStream(file);
+                        foschannel = fileOutputStream.getChannel();
+
+                        InputStream fis = resp.body().byteStream();
+                        ReadableByteChannel fisChannel = Channels.newChannel(fis);
+
+                        // 通道没有办法传输数据，必须依赖缓冲区
+                        // 分配指定大小的缓冲区
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+                        // 将通道中的数据存入缓冲区中
+                        while (fisChannel.read(byteBuffer) != -1) {  // fisChannel 中的数据读到 byteBuffer 缓冲区中
+                            byteBuffer.flip();  // 切换成读数据模式
+                            // 将缓冲区中的数据写入通道
+                            foschannel.write(byteBuffer);
+
+                            final double progress = (foschannel.size() / length);
+                            BigDecimal two = new BigDecimal(progress);
+                            double result = two.setScale(2,BigDecimal.ROUND_HALF_UP).doubleValue();
+                            //计算进度，回调
+                            if (listener != null) {
+                                listener.onProgress(result);
+                            }
+                            byteBuffer.clear();  // 清空缓冲区
+                        }
+                        foschannel.close();
+                        fisChannel.close();
+                        fileOutputStream.close();
+
+                        if (listener != null) {
+                            listener.onSuccess(file);
+                        }
                     }
+
                 } catch (IOException e) {
                     if (listener != null) {
                         listener.onError(e);
