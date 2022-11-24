df2dc24370ccc84d0ad1e8a073e8766e8acc630c
wanxingxing <wanxingxing@linewell.com>
2022-11-24 14:48:45 +0800

    :sparkles: 增加百度的短语音识别功能

--- baiduSpeechRecognition/src/main/java/com/tyky/baiduSpeechRecognition/BaiduSpeechRecognitionJsInterface.java
+++ baiduSpeechRecognition/src/main/java/com/tyky/baiduSpeechRecognition/BaiduSpeechRecognitionJsInterface.java
@@ -1,10 +1,13 @@
 package com.tyky.baiduSpeechRecognition;
 
+import android.os.Bundle;
 import android.webkit.JavascriptInterface;
 
 import com.blankj.utilcode.util.ActivityUtils;
 import com.blankj.utilcode.util.GsonUtils;
+import com.blankj.utilcode.util.StringUtils;
 import com.tyky.webviewBase.annotation.WebViewInterface;
+import com.tyky.webviewBase.model.ParamModel;
 import com.tyky.webviewBase.model.ResultModel;
 
 @WebViewInterface("baiduSpeechRecognition")
@@ -12,11 +15,19 @@ public class BaiduSpeechRecognitionJsInterface {
 
     /**
      * 语音识别
+     *
      * @return
      */
     @JavascriptInterface
     public String speechRecognition(String param) {
-        ActivityUtils.startActivity(VoiceActivity.class);
+        ParamModel paramModel = GsonUtils.fromJson(param, ParamModel.class);
+        String callBackMethod = paramModel.getCallBackMethod();
+        if (StringUtils.isEmpty(callBackMethod)) {
+            return GsonUtils.toJson(ResultModel.errorParam("callBackMethod参数未传！"));
+        }
+        Bundle bundle = new Bundle();
+        bundle.putString("callBackMethod", callBackMethod);
+        ActivityUtils.startActivity(bundle, VoiceActivity.class);
         return GsonUtils.toJson(ResultModel.success(""));
     }
 }
